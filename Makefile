e591692606c8aed87faf75f61085472e2d0c29de
shibayu36 <shibayu36@gmail.com>
2023-06-28 13:34:21 +0900

    Fix migrate.sh

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 .PHONY: migrate
 migrate:
-	mysql -u root -ppassword -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS diary"
-	mysql -u root -ppassword -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS diary_test"
-	goose -dir db/migrations mysql "root:password@(127.0.0.1:3306)/diary" up
-	goose -dir db/migrations mysql "root:password@(127.0.0.1:3306)/diary_test" up
+	./scripts/migrate.sh
 
 .PHONY: run
 run: migrate
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,13 @@
 
 go install github.com/pressly/goose/v3/cmd/goose@latest
 
+# Set default values for envvars used if not set
+DB_USER=${DB_USER:-root}
+DB_PASSWORD=${DB_PASSWORD:-password}
+DB_HOST=${DB_HOST:-127.0.0.1}
+DB_PORT=${DB_PORT:-3306}
+DB_NAME=${DB_NAME:-diary}
+
 # Run goose to MySQL using envvars which are DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
 goose -dir db/migrations mysql "$DB_USER:$DB_PASSWORD@($DB_HOST:$DB_PORT)/$DB_NAME" up
